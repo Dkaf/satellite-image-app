a9b975c064745fa44188fb8790f7c56c35214fec
Dkaf <Daniel.j.kafer@gmail.com>
2016-10-17 18:12:50 -0500

    added more styling and started work on promises

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 var assetsUrl = 'https://api.nasa.gov/planetary/earth/assets'
 var imageUrl = 'https://api.nasa.gov/planetary/earth/imagery'
-var nasaKey = 'API_KEY'
+var nasaKey = 'IIbcHBMlLJD6P8iqkyVoxNB4LDT9yt7h9rz5OPUy'
 var assetResults = [];
 var page = 1;
 var lat = 0;
 var long = 0;
+var markers = [];
+var images = [];
 
 $(document).ready(function(){
 	$('#pageForward').on('click', function(){
@@ -22,24 +24,16 @@ var fetchResults = function(){
 	$('.pageButton').show();
 	if (end > assetResults.length - 1) {
 		end = assetResults.length - 1;
+		$('#pageForward').prop('disabled', true)
+	} else {
+		$('#pageForward').prop('disabled', false)
 	}
-	for(i=start;i<=end;i++){
-		var formatDate = moment(new Date(assetResults[i].date))
-		$.ajax({
-			method: "GET",
-			url: imageUrl,
-			data: {
-				lat: lat,
-				lon: long,
-				date: formatDate.format('YYYY-MM-DD'),
-				api_key: nasaKey
-			},
-			success: function(results){
-				console.log(results);
-				$('#nasa_images').append($("<img>", {"src":results.url}));
-			}
-		})
+	if(page == 1){
+		$('#pageBack').prop('disabled', true)
+	} else {
+		$('#pageBack').prop('disabled', false)
 	}
+	$('#nasa_images').empty();
 }
 
 function initMap() {
@@ -50,37 +44,78 @@ function initMap() {
 				 zoom: 8
 			 });
 
-			 var marker = new google.maps.Marker({
-				 position: map.latLng,
-				 map: map
-			 })
 
 			 map.addListener('click', function(e){
-
-			 })
-
-			 map.addListener('click', function(e) {
+				 if(markers.length != 0){
+					deleteMarkers();
+				 }
+				 addMarker(e.latLng);
 				 lat = e.latLng.lat;
 				 long = e.latLng.lng;
-				 console.log(e.latLng.toJSON());
+			 })
+
+			 $('#submit_button').on('click', function() {
+				 console.log($('#startYear').val());
+				 //for each: push promise
+				 //promise.all
 				 $.ajax({
 					method: "GET",
   			 		url: assetsUrl,
   					data: {
-						lat: e.latLng.lat,
-						lon: e.latLng.lng,
-						// begin: startYear-01-01,
-						// end: endYear-01-01,
+						lat: lat,
+						lon: long,
+						begin: $('#startYear').val(),
+						end: $('#endYear').val(),
 						api_key: nasaKey
 					},
 					success: function(results){
 						assetResults = results.results;
-						console.log(results);
-						fetchResults();
+						console.log(results.results);
+						var imgPromises = assetResults.forEach(new Promise(function(resolve, reject) {
+							for(i=start;i<=end;i++){
+								var formatDate = moment(new Date(assetResults[i].date))
+								$.ajax({
+									method: "GET",
+									url: imageUrl,
+									data: {
+										lat: lat,
+										lon: long,
+										date: formatDate.format('YYYY-MM-DD'),
+										cloud_score: true,
+										api_key: nasaKey
+									},
+									success: function(results){
+										console.log(results);
+										if(results.cloud_score < .7){
+											images.push(results);
+										}
+									}
+								})
+							}
+						}));
+						imgPromises.then(console.log(results));
 					}
 				}).done(function() {
 						console.log("complete")
 				});
 			 })
+
+			 function addMarker(location) {
+	   	 		var marker = new google.maps.Marker({
+	 				position: location,
+	 				map: map
+	   			});
+	   			markers.push(marker);
+	 		}
+
+	 		function clearMarkers() {
+	   			marker.setMap(null);
+	 		};
+
+	 		function deleteMarkers() {
+	   			clearMarkers();
+	   			markers = [];
+	 		};
+
 
 		 }
